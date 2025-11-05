4631c9e535d9bfc673b5be3624859ddd87bbff16
mikkeline-dencker <eaa25mide@students.eaaa.dk>
2025-11-05 23:20:58 +0100

    rettelser

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function moveDodgerRight() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
+  if (left < 360) {
     dodger.style.left = `${left + 5}px`;
   }
 
